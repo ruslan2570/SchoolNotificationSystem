9f6a90f8fc4d9809d8ab29b6f813156f2e2214e5
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-05-28 10:45:28 +0300

    complete login activity

--- SchoolNotificationApp/app/src/main/java/ru/ruslan2570/schoolnotificationapp/LoginActivity.java
+++ SchoolNotificationApp/app/src/main/java/ru/ruslan2570/schoolnotificationapp/LoginActivity.java
@@ -1,8 +1,37 @@
 package ru.ruslan2570.schoolnotificationapp;
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.EditText;
 
-public class LoginActivtity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+	public static final String APP_PREFERENCES = "auth_data";
+	public static final String APP_PREFERENCES_HOST = "auth_data";
+	public static final String APP_PREFERENCES_USERNAME = "auth_data";
+	public static final String APP_PREFERENCES_PASSWORD = "auth_data";
 
 
+	SharedPreferences sp;
+	SharedPreferences.Editor editor;
+
+	EditText edHost;
+	EditText edUsername;
+	EditText edPassword;
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity);
+		edHost = findViewById(R.id.editTextHost);
+		edUsername = findViewById(R.id.editTextUsername);
+		edPassword = findViewById(R.id.editTextPassword);
+		sp = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+		editor = sp.edit();
+
+
+	}
 }
