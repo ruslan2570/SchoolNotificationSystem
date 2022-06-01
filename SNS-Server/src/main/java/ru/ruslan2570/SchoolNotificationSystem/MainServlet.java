b57712bd01320e0f39d6600be295e177abf97354
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-06-01 16:21:57 +0300

    trying to use json

--- SNS-Server/src/main/java/ru/ruslan2570/SchoolNotificationSystem/MainServlet.java
+++ SNS-Server/src/main/java/ru/ruslan2570/SchoolNotificationSystem/MainServlet.java
@@ -1,13 +1,14 @@
 package ru.ruslan2570.SchoolNotificationSystem;
 
 
-import jakarta.servlet.ServletException;
+import com.google.gson.GsonBuilder;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class MainServlet extends HttpServlet {
 
@@ -24,7 +25,7 @@ public class MainServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-        response.setContentType("text/html;charset=utf-8");
+        response.setContentType("application/json;charset=utf-8");
         String act = request.getParameter("action");
 
         try {
@@ -42,14 +43,9 @@ public class MainServlet extends HttpServlet {
                         "WHERE message.user_id = user.user_id " +
                         "AND user.role = role.role_id");
 
-                response.getWriter().println("<!DOCTYPE html>");
-                response.getWriter().println("<html>");
-                response.getWriter().println("<head>");
-                response.getWriter().println("<title>Список сообщений</title>");
-                response.getWriter().println("<style>");
-                response.getWriter().println("table { border: 1px solid grey; } th { border: 1px solid grey; } td { border: 1px solid grey; }");
-                response.getWriter().println("</style></head><body><table>");
-                response.getWriter().println("<tr><th>№</th><th>Текст</th><th>Автор</th><th>Роль автора</th>");
+                response.getWriter().println("<!DOCTYPE html><html><head><title>Список сообщений</title><style> " +
+                        "table { border: 1px solid grey; } th { border: 1px solid grey; } td { border: 1px solid grey; }" +
+                        "</style></head><body><table><tr><th>№</th><th>Текст</th><th>Автор</th><th>Роль автора</th>");
                 while (set.next()) {
                     response.getWriter().println("<tr><td>" + set.getString("msg_id") + "</td><td>" + set.getString("text")
                             + "</td><td>" + set.getString("username") + "</td><td>" + set.getString("role") + "</td></tr>");
@@ -58,7 +54,27 @@ public class MainServlet extends HttpServlet {
                 response.setStatus(HttpServletResponse.SC_OK);
             } else switch(act) {
                 case "getUsers":
+                    Statement statement = connection.createStatement();
+                    ResultSet set = statement.executeQuery("SELECT user_id, username, role.role_name FROM `user` INNER JOIN `role` ON role.role_id = user.role");
 
+                    //Gson json = new Gson();
+                    Gson json = new GsonBuilder().setPrettyPrinting().create();
+                    StringBuilder s = new StringBuilder();
+
+                    ArrayList<User> users = new ArrayList<>();
+
+                    while(set.next()){
+                        User user = new User(set.getInt("user_id"), set.getString("username"), set.getString("role_name"));
+                        response.getWriter().println(json.toJson(user));
+                        s.append(json.toJson(user));
+                    }
+
+                    users.add(json.fromJson(s.toString(), User.class));
+                    System.out.println(usr.username);
+
+
+
+                    response.setStatus(HttpServletResponse.SC_OK);
                     break;
             }
 
---
+++ SNS-Server/src/main/java/ru/ruslan2570/SchoolNotificationSystem/User.java
@@ -0,0 +1,14 @@
+package ru.ruslan2570.SchoolNotificationSystem;
+
+public class User {
+
+    public User(int userId, String username, String roleName){
+        this.userId = userId;
+        this.username = username;
+        this.roleName = roleName;
+    }
+
+    public int userId;
+    public String username;
+    public String roleName;
+}
