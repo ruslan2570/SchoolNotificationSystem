6117eb8b6f951ceb5af4a239cab4cacb1f944fce
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-06-01 12:30:07 +0300

    message out

--- SNS-Server/src/main/java/ru/ruslan2570/SchoolNotificationSystem/MainServlet.java
+++ SNS-Server/src/main/java/ru/ruslan2570/SchoolNotificationSystem/MainServlet.java
@@ -13,14 +13,32 @@ public class MainServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
+            response.setContentType("text/html;charset=utf-8");
             Connection connection = DriverManager.
 
-            Statement statement = connection.createStatement();
-            ResultSet set = statement.executeQuery("SELECT user_id, username, role.role_name FROM `user` INNER JOIN role on role.role_id = user.role");
+            getConnection("jdbc:mysql://", "user", "password");
 
+            Statement statement = connection.createStatement();
+            ResultSet set = statement.executeQuery("SELECT message.message_id as msg_id, message.text as `text`, user.username as username, " +
+                    "role.role_name as role" +
+                    " FROM `message`, `user`, `role` " +
+                    "WHERE message.user_id = user.user_id " +
+                    "AND user.role = role.role_id");
+
+            response.getWriter().println("<!DOCTYPE html>");
+            response.getWriter().println("<html>");
+            response.getWriter().println("<head>");
+            response.getWriter().println("<style>");
+            response.getWriter().println("table { border: 1px solid grey; } th { border: 1px solid grey; } td { border: 1px solid grey; }");
+            response.getWriter().println("</style></head><body><table>");
+            response.getWriter().println("<tr><th>№</th><th>Текст</th><th>Автор</th><th>Роль автора</th>");
             while(set.next()){
-                response.getWriter().println("<h2>" + set.getString("username") + "role.role_name" + "</h2>");
+                response.getWriter().println("<tr><td>" + set.getString("msg_id") + "</td><td>" + set.getString("text")
+                        + "</td><td>" + set.getString("username") + "</td><td>"  +  set.getString("role") + "</td></tr>");
             }
+            response.getWriter().println("</table></body></html>");
+            response.setStatus(HttpServletResponse.SC_OK);
+
 
         } catch (SQLException e) {
             e.printStackTrace();
