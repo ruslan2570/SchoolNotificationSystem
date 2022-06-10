255de5f706581641c7cdae317f824ca91f1db85a
Ruslan Demin <dyomin.rus@new-bokino.ru>
2022-06-10 16:35:41 +0300

    add runnable, replace icon

--- SchoolNotificationApp/app/src/main/java/ru/ruslan2570/schoolnotificationapp/LoginActivity.java
+++ SchoolNotificationApp/app/src/main/java/ru/ruslan2570/schoolnotificationapp/LoginActivity.java
@@ -2,13 +2,20 @@ package ru.ruslan2570.schoolnotificationapp;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 	public static final String APP_PREFERENCES = "auth_data";
@@ -19,6 +26,10 @@ public class LoginActivity extends AppCompatActivity {
 	SharedPreferences sp;
 	SharedPreferences.Editor editor;
 
+	private static final String TAG = "LoginActivity";
+
+	Handler handler;
+
 	EditText edHost;
 	EditText edUsername;
 	EditText edPassword;
@@ -28,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_activity);
+		handler = new Handler();
 		edHost = findViewById(R.id.editTextHost);
 		edUsername = findViewById(R.id.editTextUsername);
 		edPassword = findViewById(R.id.editTextPassword);
@@ -45,8 +57,57 @@ public class LoginActivity extends AppCompatActivity {
 			editor.putString(APP_PREFERENCES_PASSWORD, edPassword.getText().toString());
 			editor.commit();
 
-			Toast.makeText(this, "Вход", Toast.LENGTH_SHORT).show();
+			new Thread(new LoginUpdateRunnable()).start();
 
+			Toast.makeText(this, "Вход", Toast.LENGTH_SHORT).show();
 		});
 	}
+
+	public class LoginUpdateRunnable implements Runnable{
+
+		public void run(){
+			try {
+				URL url = new URL("http://" + edHost.getText() + "/request?action=login&login="
+						+ edUsername.getText() + "&password=" + edPassword.getText());
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				Log.d(TAG, "run: " + connection.getResponseCode());
+				InputStream input = connection.getInputStream();
+				byte[] buff = readAllBytes(input);
+
+				String result = new String(buff);
+
+				Log.d(TAG, "run: " + result);
+				connection.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static byte[] readAllBytes(InputStream inputStream) throws IOException {
+		final int bufLen = 1024;
+		byte[] buf = new byte[bufLen];
+		int readLen;
+		IOException exception = null;
+
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+			while ((readLen = inputStream.read(buf, 0, bufLen)) != -1)
+				outputStream.write(buf, 0, readLen);
+
+			return outputStream.toByteArray();
+		} catch (IOException e) {
+			exception = e;
+			throw e;
+		} finally {
+			if (exception == null) inputStream.close();
+			else try {
+				inputStream.close();
+			} catch (IOException e) {
+				exception.addSuppressed(e);
+			}
+		}
+	}
 }
+
+
